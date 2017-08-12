8968bfbf1b2eadd987ff8c49a3e34838b26ae8da
amberoffduty <380914555@qq.com>
2017-08-13 06:30:47 +0800

    blur header

---
+++ src/App/App.js
@@ -0,0 +1,21 @@
+import 'materialize-css/dist/css/materialize.min.css'
+import React from 'react'
+import logo from './logo.png'
+import './App.css'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="blur-wrapper">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
+                <div className='container'>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
