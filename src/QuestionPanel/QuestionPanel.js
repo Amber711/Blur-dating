da050f2bea8e3e9a659058e2992773d0fa23530f
amberoffduty <380914555@qq.com>
2017-08-16 11:55:31 +0800

    photo matches

--- src/QuestionPanel/QuestionPanel.js
+++ src/QuestionPanel/QuestionPanel.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './QuestionPanel.css'
+import refresh from '../assets/refresh.png'
 
 class QuestionPanel extends React.Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {quesitons : [
+            {
+                question: "What is essential to your morning?",
+                answer: ["Coffee", "Breakfast"]
+        },
+            {
+                question: "I consider myself athletic",
+                answer: ["Yes", "No"]
+            },
+            {
+                question: "What do you value most in a friendship?",
+                answer: ["Honesty", "Loyalty"]
+            }
+        ]}
     }
 
     componentDidMount() {
@@ -12,15 +26,17 @@ class QuestionPanel extends React.Component {
     }
 
 
-    render() {
+   render() {
        return (
            <div className="question-panel">
-            <div className="s-box"></div>
-            <div className="q-wrapper">
-                <p className="question">What do you value most in a friendship?</p>
-                <button className="q-btn-left">Honesty</button>
-                <button className="q-btn-rt">Loyalty</button>
-            </div>
+                <img className="refresh" src={refresh} alt=""/>
+                <div className="s-box"></div>
+                <div className="q-wrapper">
+                    <p className="question">{this.state.quesitons.question}</p>
+                    <button className="q-btn-left">{tos.}</button>
+                    <button className="q-btn-rt">Loyalty</button>
+                </div>
+                 <button className="skip-q">SKIP THIS QUESTION</button>
            </div>
        )
     }
